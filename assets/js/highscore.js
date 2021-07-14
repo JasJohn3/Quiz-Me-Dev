097d66bb77748a02a227fc9c65eb57d0e1b7a0a7
Your Name <jasjohn3@uat.edu>
2021-07-14 06:12:09 -0700

    HighScores Segment

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,10 +1,32 @@
-var players = JSON.parse(localStorage.getItem('player-info'));
-
+let players = JSON.parse(localStorage.getItem('player-info'));
+let ul = document.querySelector('#top-scores')
 console.log(players);
 
-Object.entries(players).sort((a,b)=>b[1]-a[1]);
-console.log(players);
+console.log(ul);
 
 createHighScoreUI = ()=>{
-  
+  players.forEach(item => {
+    let li = document.createElement('li');
+    li.classList = 'choice-container';
+    
+    if(players[0]){
+      li.innerHTML = 
+      `
+      <i class="fas fa-crown"></i>
+      <p class="choice-prefix">Name: ${item.name}</p>
+      <p class="choice-text">Score: ${item.score}</p>
+      `;
+    }else{
+      li.innerHTML = 
+      `
+      <i class="fas fa-gem"></i>
+      <p class="choice-prefix">Name: ${item.name}</p>
+      <p class="choice-text">Score: ${item.score}</p>
+      `;
+    }
+    console.log(li);
+    ul.append(li);
+  });
 }
+
+createHighScoreUI();
